959cb058d1fdf6b544b3a92ff4976da52a168340
cbl <lennart.carbe@gmail.com>
2020-07-08 15:01:22 +0200

    dependencies

--- src/RouteServiceProvider.php
+++ src/RouteServiceProvider.php
@@ -3,8 +3,8 @@
 namespace FjordPermissions;
 
 use Fjord\Support\Facades\Package;
-use FjordPermissions\Controllers\RoleController;
 use FjordPermissions\Controllers\PermissionController;
+use FjordPermissions\Controllers\RoleController;
 use FjordPermissions\Controllers\RolePermissionController;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as LaravelRouteServiceProvider;
 
@@ -32,9 +32,9 @@ class RouteServiceProvider extends LaravelRouteServiceProvider
     public function addNavPresets()
     {
         $this->package->addNavPreset('permissions', [
-            'link' => route('fjord.aw-studio.fjord-permissions.permissions'),
-            'title' => __f('fj.permissions'),
-            'icon' => fa('unlock-alt'),
+            'link'      => route('fjord.aw-studio.fjord-permissions.permissions'),
+            'title'     => fn ()     => __f('fj.permissions'),
+            'icon'      => fa('unlock-alt'),
             'authorize' => function ($user) {
                 return $user->can('read fjord-role-permissions');
             },
@@ -44,31 +44,31 @@ class RouteServiceProvider extends LaravelRouteServiceProvider
     protected function mapRolePermissionRoutes()
     {
         $route = $this->package->route()
-            ->get('/permissions', PermissionController::class . '@index')
+            ->get('/permissions', PermissionController::class.'@index')
             ->name('permissions');
 
         $this->package->route()
-            ->post('/fjord-user/{user_id}/role/{role_id}', RoleController::class . '@assignRoleToUser')
+            ->post('/fjord-user/{user_id}/role/{role_id}', RoleController::class.'@assignRoleToUser')
             ->name('role.assign');
 
         $this->package->route()
-            ->delete('/fjord-user/{user_id}/role/{role_id}', RoleController::class . '@removeRoleFromUser')
+            ->delete('/fjord-user/{user_id}/role/{role_id}', RoleController::class.'@removeRoleFromUser')
             ->name('role.remove');
 
         $this->package->route()
-            ->post('/role', RoleController::class . '@store')
+            ->post('/role', RoleController::class.'@store')
             ->name('role.store');
 
         $this->package->route()
-            ->delete('/role/{id}', RoleController::class . '@destroy')
+            ->delete('/role/{id}', RoleController::class.'@destroy')
             ->name('role.destroy');
 
         $this->package->route()
-            ->post('/index', PermissionController::class . '@fetchIndex')
+            ->post('/index', PermissionController::class.'@fetchIndex')
             ->name('permissions.index');
 
         $this->package->route()
-            ->put('/role_permissions', RolePermissionController::class . '@update')
+            ->put('/role_permissions', RolePermissionController::class.'@update')
             ->name('role_permissions.update');
     }
 }
